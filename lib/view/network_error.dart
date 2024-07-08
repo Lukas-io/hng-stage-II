8835987e67efb339e1ba65d26818d1c214f502c1
Lukas-io <wisdomiyamu@gmail.com>
2024-07-08 16:49:11 +0100

    HNG STAGE II✅✅✅

---
+++ lib/view/network_error.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class NetworkError extends StatelessWidget {
+  const NetworkError({
+    super.key,
+    required this.error,
+    required this.onTryAgain,
+  });
+
+  final String error;
+
+  final void Function() onTryAgain;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                CupertinoIcons.wifi_slash,
+                color: CupertinoColors.systemOrange.highContrastElevatedColor
+                    .withOpacity(0.4),
+                size: 100.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 12.0),
+                child: const Text(
+                  'Try Again!',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                error.replaceAll('Exception: ', ''),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 30.0),
+              ElevatedButton(
+                onPressed: onTryAgain,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: CupertinoColors
+                      .systemOrange.highContrastElevatedColor
+                      .withOpacity(0.7),
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                child: const Text(
+                  'Retry',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
