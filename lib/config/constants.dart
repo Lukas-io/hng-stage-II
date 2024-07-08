620e2632ec32afb08112ed3175063bd33cc9c917
Lukas-io <120050725+Lukas-io@users.noreply.github.com>
2024-07-08 09:05:41 -0700

    get a timmbu account

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,5 +1,5 @@
 const String kApikey = <YOUR-API-KEY-HERE>;
-const String kOrganisationId = "b6df64c20a46410b86854fb24de5960d";
-const String kAppId = "A4POZ0FOF3RI8WU";
+const String kOrganisationId = <YOUR-ORG-ID>;
+const String kAppId = <YOUR-API-ID>;
 
 const String kBaseApiUrl = "https://api.timbu.cloud";
