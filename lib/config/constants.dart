2c835e5f512fc2c53f11d1bfee2ba5057aaab38a
Lukas-io <120050725+Lukas-io@users.noreply.github.com>
2024-07-08 09:04:34 -0700

    get a timbu api key

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,4 @@
-const String kApikey = "b8edab2540184eda8cf197d814467eac20240706172504466789";
+const String kApikey = <YOUR-API-KEY-HERE>;
 const String kOrganisationId = "b6df64c20a46410b86854fb24de5960d";
 const String kAppId = "A4POZ0FOF3RI8WU";
 
